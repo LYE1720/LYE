1de8c08cc2b2ad865aad1bc8c9f46a10d176450f
LYE1720 <tienyuanloli@gmail.com>
2025-12-07 14:10:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -165,7 +165,7 @@ function setupLetter(){
   const map = {
     openLetterBtn: "letter",   // 第一顆 → #letter
     openNote1:     "letter2",  // 第二顆 → #letter2
-    openNote2:     "letter3"   // 第三顆 → #letter3
+    openNote2:     "letter3",   // 第三顆 → #letter3
     openNote3:     "letter4"
   };
   Object.entries(map).forEach(([btnId, dlgId])=>{
